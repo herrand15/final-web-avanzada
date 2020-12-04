7073736d2cca550b433877dff922dfb32431af4d
herrand15 <20165239@ce.pucmm.edu.do>
2020-12-03 21:35:55 -0400

    examen flnal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Weather from "./components/weather"
+import Event from "./components/event"
 
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <Event />
     </div>
   );
 }
